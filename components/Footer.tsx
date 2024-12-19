432fa1f781edfec4a0d532ed5e674a2f9260b3f1
“Ignacio” <revuelta.ig@gmail.com>
2024-12-19 16:56:42 +0100

    fix: images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,12 @@ export default function Footer() {
     <footer className="mx-32 flex items-center justify-between px-64 py-32 border-t border-t-blue-400_50 rounded-t-32 m:flex-col m:gap-32 m:px-32">
       <div className="flex items-center gap-24 m:flex-col m:text-center">
         <a href="https://iamnacho.com" target="_blank">
-          <Image src="logoFooter.svg" alt="I Am Nacho" />
+          <Image
+            src="logoFooter.svg"
+            alt="I Am Nacho"
+            width="48"
+            height="37"
+          />
         </a>
 
         <p className="text-gray-400 leading-5 m:leading-6">
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,13 @@ export default function Header() {
     <>
       <header className="fixed backdrop-blur-sm z-50 bg-black_50 w-full px-64 py-32 border-b border-b-blue-400_10 m:px-32">
         <div className="mx-auto flex items-center justify-between max-w-1400">
-          <Image src="logo.svg" alt="Movinwords" className="h-48 m:h-24" />
+          <Image
+            src="logo.svg"
+            className="h-48 m:h-24"
+            alt="Movinwords"
+            width="184"
+            height="48"
+          />
 
           <div className="flex items-center gap-24 m:gap-12">
             <a href="https://github.com/revueltai/movinwords" target="_blank">
@@ -14,14 +20,18 @@ export default function Header() {
                 src="https://img.shields.io/badge/license-MIT-blue.svg"
                 className="m:h-12"
                 alt="MIT Licence"
+                width="80"
+                height="20"
               />
             </a>
 
             <a href="https://github.com/revueltai/movinwords" target="_blank">
               <Image
-                src="https://img.shields.io/npm/v/movinwords"
+                src="https://img.shields.io/npm/v/movinwords.svg"
                 className="m:h-12"
                 alt=""
+                width="80"
+                height="20"
               />
             </a>
 
