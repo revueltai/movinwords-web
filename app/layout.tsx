9b61ece6728a242caac6dbe8a0522eec66519fa6
“Ignacio” <revuelta.ig@gmail.com>
2024-12-28 19:09:11 +0100

    feat: added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from 'next'
 import './globals.css'
 import 'movinwords/styles'
@@ -30,6 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   )
